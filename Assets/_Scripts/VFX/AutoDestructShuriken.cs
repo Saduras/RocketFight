1e67b36fa81049565b01c67e034fe8e0a5f0e420
David Speck <david@saduras.de>
2013-06-06 10:26:08 +0200

    Apply VFX explosion to projectil & change AutoDestruct to Networt version

---
+++ Assets/_Scripts/VFX/AutoDestructShuriken.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AutoDestructShuriken : Photon.MonoBehaviour {
+	
+	public bool OnlyDeactivate;
+	
+	void OnEnable()
+	{
+		StartCoroutine("CheckIfAlive");
+	}
+	
+	IEnumerator CheckIfAlive ()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(0.5f);
+			if(!particleSystem.IsAlive(true))
+			{
+				if(OnlyDeactivate)
+				{
+					#if UNITY_4_0
+							this.gameObject.SetActive(false);
+					#elif UNITY_3_5
+							this.gameObject.SetActiveRecursively(false);
+					#endif
+				}
+				else
+					PhotonNetwork.Destroy(this.gameObject);
+				break;
+			}
+		}
+	}
+}
