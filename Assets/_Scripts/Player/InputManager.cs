17f200968e3de24aac23b4e37384223931fc157a
David Speck <david@saduras.de>
2013-06-11 17:03:32 +0200

    Attach RocketLauncher to Character; Look in shot direction for short time, after shot

--- Assets/_Scripts/Player/InputManager.cs
+++ Assets/_Scripts/Player/InputManager.cs
@@ -15,6 +15,7 @@ public class InputManager : Photon.MonoBehaviour {
 	public GameObject muzzleFlash;
 	public string groundTag = "Ground";
 	private float lastShot = 0;
+	private Vector3 shotDir = Vector3.forward;
 	
 	private Animator anim;
 	
@@ -45,63 +46,68 @@ public class InputManager : Photon.MonoBehaviour {
 	public void Update () {
 		// Check for input updates
 		if( (PhotonNetwork.player == controllingPlayer) ) {
-			// Get movement input.
-			Vector3 movement = Vector3.zero;
-			Vector3 hitPoint;
-			switch(moveControl) {
-				case Controls.keyboard:
-					// calculate movement vector from keyboard input
-					Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
-					movement = new Vector3(moveDirection.x, 0, moveDirection.y);
-					movement.Normalize();
-					break;
-				case Controls.mouse:
-					// update destination (projected to the x-z-plane)
-					if( Input.GetButton("Fire2") ) {
-						moveTo = GetMouseHitPoint();
-					}
-					// project current position tp the x-z plane
-					Vector3 pos = this.transform.position;
-					pos.y = 0;
-					moveTo.y = 0;
-					// calculate movement vector to destination if we are not already there
-					if( Vector3.Distance(moveTo,pos) > moveEpsilon ) {
-						movement = moveTo - pos;
-						movement.y = 0;
+			if( Time.time - lastShot < 0.2f ) {
+				transform.LookAt( shotDir );	
+			} else {
+				// Get movement input.
+				Vector3 movement = Vector3.zero;
+				Vector3 hitPoint;
+				switch(moveControl) {
+					case Controls.keyboard:
+						// calculate movement vector from keyboard input
+						Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+						movement = new Vector3(moveDirection.x, 0, moveDirection.y);
 						movement.Normalize();
-					} else {
-						movement = Vector3.zero;					
-					}
-					break;
-			}
-			// apply previous calculated movement
-			this.transform.Translate(movement * speed * Time.deltaTime, Space.World);
-			anim.SetFloat("speed", movement.magnitude );
-			
-			// Get rotation input.
-			hitPoint = GetMouseHitPoint();
-			Vector3 viewDirection = hitPoint - this.transform.position;
-			viewDirection.y = 0;
-			viewDirection.Normalize();
-			if( movement.magnitude > 0.1)
-				this.transform.LookAt(this.transform.position + movement);
-
-			if( Input.GetButton("Fire1") ) {
-				if( Time.time > lastShot + cooldown ) {
-					Vector3 pos = this.transform.position + viewDirection.normalized + Vector3.up * 0.5f;
-					PhotonNetwork.Instantiate(muzzleFlash.name,
-												pos, 
-												this.transform.rotation, 0);
-					GameObject handle = PhotonNetwork.Instantiate(projectile.name, 
-												pos, 
-												this.transform.rotation, 0);
-					// handle.transform.LookAt( handle.transform.position + viewDirection );
-					// hack for strange model...
-					handle.transform.LookAt( handle.transform.position - viewDirection );
-					
-					handle.SendMessage("SetRange", Vector3.Distance(pos, hitPoint) );
-					lastShot = Time.time;
+						break;
+					case Controls.mouse:
+						// update destination (projected to the x-z-plane)
+						if( Input.GetButton("Fire2") ) {
+							moveTo = GetMouseHitPoint();
+						}
+						// project current position tp the x-z plane
+						Vector3 pos = this.transform.position;
+						pos.y = 0;
+						moveTo.y = 0;
+						// calculate movement vector to destination if we are not already there
+						if( Vector3.Distance(moveTo,pos) > moveEpsilon ) {
+							movement = moveTo - pos;
+							movement.y = 0;
+							movement.Normalize();
+						} else {
+							movement = Vector3.zero;					
+						}
+						break;
 				}
+				// apply previous calculated movement
+				this.transform.Translate(movement * speed * Time.deltaTime, Space.World);
+				anim.SetFloat("speed", movement.magnitude );
+				
+				// Get rotation input.
+				hitPoint = GetMouseHitPoint();
+				Vector3 viewDirection = hitPoint - this.transform.position;
+				viewDirection.y = 0;
+				viewDirection.Normalize();
+				if( movement.magnitude > 0.1)
+					this.transform.LookAt(this.transform.position + movement);
+	
+				if( Input.GetButton("Fire1") ) {
+					if( Time.time > lastShot + cooldown ) {
+						Vector3 pos = this.transform.position + viewDirection.normalized + Vector3.up * 0.5f;
+						PhotonNetwork.Instantiate(muzzleFlash.name,
+													pos, 
+													this.transform.rotation, 0);
+						GameObject handle = PhotonNetwork.Instantiate(projectile.name, 
+													pos, 
+													this.transform.rotation, 0);
+						// handle.transform.LookAt( handle.transform.position + viewDirection );
+						// hack for strange model...
+						handle.transform.LookAt( handle.transform.position - viewDirection );
+						
+						handle.SendMessage("SetRange", Vector3.Distance(pos, hitPoint) );
+						lastShot = Time.time;
+						shotDir = handle.transform.position + viewDirection;
+					}
+			}
 			}
 		}
 	}
