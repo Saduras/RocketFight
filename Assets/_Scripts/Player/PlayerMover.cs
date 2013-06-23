30920b56677d34e404cf0297d873154794b87c6f
David Speck <david@saduras.de>
2013-06-23 17:18:12 +0200

    Comment last changes in PlayerMover

--- Assets/_Scripts/Player/PlayerMover.cs
+++ Assets/_Scripts/Player/PlayerMover.cs
@@ -6,22 +6,28 @@ using System.Collections;
  */
 public class PlayerMover : MonoBehaviour {
 	
+	// direction we get from the player input
 	Vector3 inputMovement = Vector3.zero;
+	// sum of all forces which influence the character now
 	Vector3 physicMovement = Vector3.zero;
+	
+	// speed of the character movement through player input
 	public float movementSpeed = 5;
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+
+	/**
+	 * Update is called once per frame
+	 * Apply movement from input and physics to the player
+	 */
 	void Update () {
 		Vector3 frameMove = inputMovement * movementSpeed;
 		frameMove += physicMovement;
 		transform.Translate( frameMove * Time.deltaTime,Space.World);
 	}
 	
+	/**
+	 * Set the vector value of inputMovement.
+	 * It will be normalized, if it isn't already since we only use the direction.
+	 */
 	public void SetInputMovement( Vector3 vector ) {
 		if( vector.magnitude != 1 )
 			vector.Normalize();
@@ -30,11 +36,17 @@ public class PlayerMover : MonoBehaviour {
 			inputMovement = vector;
 	}
 	
+	/**
+	 * Set the sum of the physical effects moving the character.
+	 */
 	public void SetPhysicMovement( Vector3 vector ) {
 		if( vector != physicMovement )
 			physicMovement = vector;
 	}
 	
+	/**
+	 * Immediatly set the charater to a new position.
+	 */
 	public void Teleport( Vector3 pos ) {
 		transform.position = pos;	
 	}
