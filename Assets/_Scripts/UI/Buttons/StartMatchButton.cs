61f043472be623a02113e6317f77f847cf33756d
David Speck <david@saduras.de>
2013-07-26 11:51:41 +0200

    Hide start-match button properly on 2ndary clients

--- Assets/_Scripts/UI/Buttons/StartMatchButton.cs
+++ Assets/_Scripts/UI/Buttons/StartMatchButton.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class StartMatchButton : MonoBehaviour {
+	
+	public GameObject targetBackground;
+	public GameObject targetSprite;
 
 	void Update() {
 		// TODO: doenst hide properly with new prefab
 		if(PhotonNetwork.isMasterClient && PhotonNetwork.connectionStateDetailed == PeerState.Joined) {
-			GetComponent<UISprite>().enabled = true;
-			GetComponent<BoxCollider>().enabled = true;
+			targetBackground.GetComponent<UISprite>().enabled = true;
+			targetBackground.GetComponent<BoxCollider>().enabled = true;
+			targetSprite.GetComponent<UISprite>().enabled = true;
 		} else {
-			GetComponent<UISprite>().enabled = false;
-			GetComponent<BoxCollider>().enabled = false;
+			targetBackground.GetComponent<UISprite>().enabled = false;
+			targetBackground.GetComponent<BoxCollider>().enabled = false;
+			targetSprite.GetComponentInChildren<UISprite>().enabled = false;
 		}
 	}
 	
